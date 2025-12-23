371c5f1af3f43b6756a47c961e00a877253d972d
Wesley Jones <wesjones2004@gmail.com>
2025-12-22 19:19:27 -0500

    Add MemoryMapRequest and update BASE_REVISION initialization in limine.rs

--- src/limine.rs
+++ src/limine.rs
@@ -1,17 +1,24 @@
 use limine::{
     BaseRevision,
-    request::{FramebufferRequest, RequestsEndMarker, RequestsStartMarker},
+    request::{FramebufferRequest, MemoryMapRequest, RequestsEndMarker, RequestsStartMarker},
 };
+use log::info;
+
+pub const KERNEL_BASE: usize = 0xffffffff80000000;
 
 /// Sets the base revision to the latest revision supported by the crate.
 /// See specification for further info.
 #[used]
 #[unsafe(link_section = ".requests")]
-pub static BASE_REVISION: BaseRevision = BaseRevision::new();
+pub static BASE_REVISION: BaseRevision = BaseRevision::with_revision(0);
 
 #[used]
 #[unsafe(link_section = ".requests")]
 pub static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
+
+#[used]
+#[unsafe(link_section = ".requests")]
+pub static MEMORY_MAP_REQUEST: MemoryMapRequest = MemoryMapRequest::new();
 
 /// Define the stand and end markers for Limine requests.
 #[used]
@@ -21,3 +28,14 @@ static _START_MARKER: RequestsStartMarker = RequestsStartMarker::new();
 #[used]
 #[unsafe(link_section = ".requests_end_marker")]
 static _END_MARKER: RequestsEndMarker = RequestsEndMarker::new();
+
+pub fn validate_limine_version() {
+    if !BASE_REVISION.is_supported() {
+        panic!("Limine bootloader version not supported");
+    } else {
+        info!(
+            "AtahOS Kernel Initialized - Limine Revision: {}",
+            BASE_REVISION.loaded_revision().unwrap()
+        )
+    }
+}
